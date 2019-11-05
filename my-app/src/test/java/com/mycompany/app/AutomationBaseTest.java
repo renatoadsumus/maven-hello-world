b58ecf6bf392e394ac2e8dc86ae53307a6bb7c21
EC2 Default User <ec2-user@ip-172-31-92-84.ec2.internal>
2019-11-05 18:54:22 +0000

    Incluindo classe de teste para Appium

---
+++ my-app/src/test/java/com/mycompany/app/AutomationBaseTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import junit.framework.*;
+
+public class AutomationBaseTest extends TestCase{
+    
+    //public static AndroidDriver driver;
+    
+	@Test
+    public void testConcatenate() {
+
+        
+
+        assertEquals("onetwo", "onetwo");
+
+    }
+
+}
